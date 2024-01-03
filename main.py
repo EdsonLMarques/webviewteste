63127a23d4ef9fd2ae0f3339e3be060e1d3e9a12
Edson <edsonmarques@gea.inatel.br>
2024-01-03 11:40:24 -0300

    teste

--- main.py
+++ main.py
@@ -1,56 +1,25 @@
 import webview
+import time
+from API import API
 
-class API:
-    def load_inputs(self):
-        # Adicione aqui o código para carregar os inputs
-        print('clicou')
-        response = {
-            'message': 'Load Inputs'
-        }
-        return response
-
-    def generate_report(self):
-        # Adicione aqui o código para gerar o relatório
-        response = {
-            'message': 'generate report'
-        }
-        return response
-
-    def clear_all(self):
-        # Adicione aqui o código para limpar tudo
-        response = {
-            'message': 'clear all'
-        }
-        return response
-
-    def generate_log(self):
-        # Adicione aqui o código para gerar o log
-        response = {
-            'message': 'generate log'
-        }
-        return response
-
-    def printar(self, mensagem):
-        '''
-        Printar apenas para testes, informações vindas do HTML
-        :param mensagem:
-        :return:
-        '''
-        print(mensagem)
+def load_html(window):
+    with open('src/index.html', 'r') as file:
+        html_content = file.read()
+    time.sleep(1)
+    window.load_html(html_content)
 
 def main():
-    with open('index.html', 'r') as file:
+    with open('src/components/home_screen.html', 'r') as file:
         html_content = file.read()
     api = API()
     window = webview.create_window(
-        title="Sua Ferramenta",
+        title="Teste pywebview",
         html=html_content,
         js_api=api,
         width=800,
         height=600
     )
-
-    webview.start(debug=False)
+    webview.start(load_html, window)
 
 if __name__ == '__main__':
     main()
---
+++ readme.txt
@@ -0,0 +1,14 @@
+anotações sobre pywebview
+
+exutando ações a partir do front utilizar o seguinte passo a passo
+    criar classe
+    na classe, criar os esqueletos das funções (chamada de codigo + retorno)
+
+    no html, criar uma função js
+        dentro da função, executar o comando pywebview.api.NOME_DA_FUNÇAO isso ira executar a funçao esqueleto em python
+
+
+
+a função evaluate_js serve para executar um codigo JS a partir do python, o codigo deve estar no formato string
+
+a função expose serve para criar mais uma conexao de API dessa forma é possivel gearar
